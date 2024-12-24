2a7136b3a59b369ed6d710496c1e27dd7535003c
Elijah Passmore <elijah.passmore@gnometeaparty.com>
2024-12-25 09:05:44 +1300

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# astro-cloudflare-pages-template
+
+This is a template for Astro Cloudflare Pages.
+
+## Getting Started
+
+1. Rename the files in the project to the name of your app.
+
+   ```bash
+   pnpx replace-in-file "astro-cloudflare-pages-template/g" "MY_APP" "./**/*.*" --verbose
+   pnpx replace-in-file "astro-cloudflare-pages-template.gtp.dev/g" "MY_DOMAIN" "./**/*.*" --verbose
+   ```
+
+2. Add the required environment variables to the `.env` file (see `.env.example` for reference).
+
+3. Install dependencies
+
+   ```bash
+   pnpm i
+   ```
---
+++ astro.config.mjs
@@ -0,0 +1,25 @@
+// @ts-check
+// @ts-check
+import { defineConfig } from "astro/config";
+
+import cloudflare from "@astrojs/cloudflare";
+import parytown from "@astrojs/partytown";
+import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://astro-cloudflare-pages-template.gtp.dev",
+  adapter: cloudflare({
+    platformProxy: {
+      enabled: true,
+    },
+  }),
+  integrations: [parytown(), sitemap(), tailwind()],
+  experimental: {
+    responsiveImages: true,
+    svg: {
+      mode: "inline",
+    },
+  },
+});
